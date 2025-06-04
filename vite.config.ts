5fcd49e236aad8e50865a78f04ad515a4145b42d
MiniHapz <hafizhalrizky19.9b@gmail.com>
2025-06-04 12:14:57 +0700

    ntahlah

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,23 @@ import legacy from '@vitejs/plugin-legacy'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     legacy()
   ],
+  build: {
+    chunkSizeWarningLimit: 1000, // Supaya warning muncul kalau > 1MB
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          // Misalnya kamu pakai library besar, bisa dipisah
+          react: ['react', 'react-dom'],
+          vendor: ['axios', 'lodash'] // Tambah sesuai library kamu
+        }
+      }
+    }
+  },
   test: {
     globals: true,
     environment: 'jsdom',
